5f47366f5b01bf82f60bcda2f4f778468af5538f
kaszanka1234 <adriankodzis@op.pl>
2019-04-08 18:13:47 +0200

    removed unused class

--- ClanSpecific/Load.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace THONK.ClanSpecific{
-    public class Load{
-        public void ConfigureClanServices(ServiceCollection services){}
-    }
-}
