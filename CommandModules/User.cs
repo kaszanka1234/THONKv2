c818635ddb9a70a86fb5af8f9e46fd6c7fc9f462
kaszanka1234 <adriankodzis@op.pl>
2019-04-17 14:46:48 +0200

    finished /user command

--- CommandModules/User.cs
+++ CommandModules/User.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -16,10 +18,43 @@ namespace THONK.CommandModules{
         }
 
         // show possible completions for command
-        [Command(""),Priority(-1)]
+        [Command(""),Priority(0)]
         public async Task Usage([Remainder]string s=""){
-            string prefix = _config[Context.Guild.Id].Prefix;
-            string msg = $"";
+            string p = _config[Context.Guild.Id].Prefix;
+            string msg = $"correct commands:\n{p}user approve\n{p}user rank\n{p}user mr";
+            await Context.Channel.SendMessageAsync(msg);
+        }
+
+        // approve user as clan member
+        [Command("approve"),Alias("accept","a")]
+        public async Task Approve(SocketGuildUser user){
+            // get visitor role
+            SocketRole visitor = Context.Guild.Roles.Where(x=>x.Name=="Visitor").First();
+            // check if user has visitor role (if can be approved)
+            if(user.Roles.Contains(visitor)){
+                // add initiate role and remove visitor role
+                await user.AddRoleAsync(Context.Guild.Roles.Where(x=>x.Name=="Initiate").First());
+                await user.RemoveRoleAsync(visitor);
+
+                // send a message signalig success
+                string msg = $"**{(string.IsNullOrEmpty(user.Nickname)?user.Username:user.Nickname)}** is now member of the clan, welcome and have fun!";
+                await Context.Channel.SendFileAsync(msg);
+
+                // send a message to botlog channel
+                var channel = _config[Context.Guild.Id].BotLogChannel;
+                if(channel!=null){
+                    msg = $"User {user.Mention} ({user.Id}) was approved by {Context.User.Mention}";
+                    await channel.SendMessageAsync(msg);
+                }
+            }else{
+                await Context.Channel.SendMessageAsync("User is already approved");
+            }
+        }
+
+        // show usage for command
+        [Command("approve"),Alias("accept","a"),Priority(1)]
+        public async Task Approve(){
+            string msg = $"Correct syntax:\n{_config[Context.Guild.Id].Prefix}user approve (@user)";
             await Context.Channel.SendMessageAsync(msg);
         }
 
@@ -27,22 +62,115 @@ namespace THONK.CommandModules{
         [Command("rank")]
         public async Task Rank(SocketGuildUser user, string rank){
             var userRequesting = Context.User as SocketGuildUser;
-            if(userRequesting.Authorized("Lieutenant")){
-                await InsufficientPermissions();
-                return;
-            }
-            if(!userRequesting.HigherThan(user)){await InsufficientPermissions();return;}
+            // disallow users with rank lower than Lieutenant
+            if(!userRequesting.Authorized("Lieutenant")){await InsufficientPermissionsAsync();return;}
+
+            // disallow changing roles of users with higher rank
+            if(!userRequesting.HigherThan(user)){await InsufficientPermissionsAsync();return;}
+
+            SocketRole role;
+            string name;
             
-            // TODO set rank
+            // match clan ranks with entered text and change any
+            // combination of big and small letters to just
+            // the first letter as big
+            switch(rank.ToLower()){
+                case "warlord":     name = "Warlord";break;
+                case "general":     name = "General";break;
+                case "lieutenant":  name = "Lieutenant";break;
+                case "sergeant":    name = "Sergeant";break;
+                case "soldier":     name = "Soldier";break;
+                case "guest":       name = "Guest";break;
+                case "initiate":    name = "Initiate";break;
+                default:            name = "notfound";break;
+            }
+
+            // disallow setting roles higher than own
+            if(!userRequesting.Authorized(name)){await InsufficientPermissionsAsync();return;}
+            
+            // if passed role is correct clan rank set it as user role
+            // and delete any other clan roles
+            if(name != "notfound"){
+                role = Context.Guild.Roles.Where(x=>x.Name==name).First();
+                await user.AddRoleAsync(role);
+                await user.DeleteClanRanksExceptAsync(name);
+                string msg = $"Rank of {(string.IsNullOrEmpty(user.Nickname)?user.Username:user.Nickname)} was set to {role.Name}";
+                await Context.Channel.SendMessageAsync(msg);
+            }
+            // if role isn't clan rank try to match it with any other assignable role
+            else{
+                await RankOther(user,rank);
+            }
         }
 
         // overload for previous command
         [Command("rank")]
         public async Task Rank(string rank,SocketGuildUser user)=>await Rank(user,rank);
+
+        // show usage for command
+        [Command("rank"),Priority(1)]
+        public async Task Rank(){
+            string msg = $"Correct syntax:\n{_config[Context.Guild.Id].Prefix}user rank (@user) (rank)";
+            await Context.Channel.SendMessageAsync(msg);
+        }
         
+        // method for setting roles not releated to clan ranks
+        private async Task RankOther(SocketGuildUser user, string rank){
+            if(false){
+                // placeholder for setting other roles
+            }else{
+                string msg = "Unknown role";
+                await Context.Channel.SendMessageAsync(msg);
+            }
+        }
+
+        // set mastery rank for user
+        [Command("")]
+        public async Task Mr([Remainder]string rank){
+            int r;
+            bool succ;
+            string msg;
+
+            // check if rank starts with "mr" and parse number of the rank
+            if(rank.StartsWith("mr ",true,null)){
+                succ = int.TryParse(rank.Substring(3),out r);
+            }else if(rank.StartsWith("mr",true,null)){
+                succ = int.TryParse(rank.Substring(2),out r);
+            }else{
+                await Usage();
+                return;
+            }
+            if(succ&&r>=0&&r<=30){
+                await (Context.User as SocketGuildUser).RemoveRolesAsync((Context.User as SocketGuildUser).Roles.Where(x=>x.Name.Substring(0,2)=="MR"));
+                await (Context.User as SocketGuildUser).AddRoleAsync(Context.Guild.Roles.Where(x=>x.Name==$"MR{r}").First());
+                msg = $"{(string.IsNullOrEmpty((Context.User as SocketGuildUser).Nickname)?Context.User.Username:(Context.User as SocketGuildUser).Nickname)} has been assigned MR{r}";
+                await Context.Channel.SendMessageAsync(msg);
+            }else{
+                string p = _config[Context.Guild.Id].Prefix;
+                msg = $"Something went wrong, try something like {p}user mr 15";
+                await Context.Channel.SendMessageAsync(msg);
+            }
+        }
+
         // show insufficient permissions
-        public async Task InsufficientPermissions(){
+        private async Task InsufficientPermissionsAsync(){
             await Context.Channel.SendMessageAsync(":x: Insufficient Permissions");
+        }
+    }
+    public static class UserExtensions{
+        // SocketGuildUser extension to allow deleting all clan related roles except
+        // the one given in argument
+        public static async Task DeleteClanRanksExceptAsync(this SocketGuildUser user, string role){
+            List<string> roles = new List<string>();
+            roles.Add("Initiate");
+            roles.Add("Guest");
+            roles.Add("Soldier");
+            roles.Add("Sergeant");
+            roles.Add("Lieutenant");
+            roles.Add("General");
+            roles.Add("Warlord");
+            roles.Remove(role);
+            await user.RemoveRolesAsync(user.Roles.Where(x=>roles.Contains(x.Name)));
         }
     }
 }
--- Extensions/SocketGuildUserExtension/Extension.cs
+++ Extensions/SocketGuildUserExtension/Extension.cs
@@ -7,43 +7,35 @@ namespace THONK.Extensions.SocketGuildUserExtension{
     public static class Extension{
         
         public static bool Authorized(this SocketGuildUser user, string minRole){
-            return user.AccessLevel()<=RoleToInt(minRole);
+            return user.AccessLevel()>=RoleToInt(minRole);
         }
 
         private static int RoleToInt(string role){switch (role){
             case "Warlord":
-                return 0;
+                return 10;
             case "General":
-                return 1;
+                return 9;
             case "Lieutenant":
-                return 2;
+                return 8;
             case "Sergeant":
-                return 3;
+                return 6;
             case "Soldier":
                 return 4;
             case "Guest":
-                return 5;
+                return 3;
             case "Initiate":
-                return 6;
+                return 2;
             default:
-                return 7;
+                return 0;
         }}
 
         public static int AccessLevel(this SocketGuildUser user){
-            int accessLevel = 7;
-            List<string> roleList = new List<string>();
-            roleList.Insert(0,"Warlord");
-            roleList.Insert(1,"General");
-            roleList.Insert(2,"Lieutenant");
-            roleList.Insert(3,"Sergeant");
-            roleList.Insert(4,"Soldier");
-            roleList.Insert(5,"Guest");
-            roleList.Insert(6,"Initiate");
+            //if(user.Guild.Owner == user)return 11;
+            int accessLevel = 0;
             foreach(var role in user.Roles){
-                if(roleList.Contains(role.Name)){
-                    if(accessLevel>roleList.IndexOf(role.Name)){
-                        accessLevel = roleList.IndexOf(role.Name);
-                    }
+                int tmp = RoleToInt(role.Name);
+                if(tmp>accessLevel){
+                    accessLevel = tmp;
                 }
             }
             return accessLevel;
