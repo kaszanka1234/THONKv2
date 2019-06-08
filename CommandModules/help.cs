56d80d7e6e54d1656adb61e55b665bce07a634e5
Kaszanka1234 <adriankodzis@op.pl>
2019-06-08 13:13:27 +0200

    added help command

--- CommandModules/help.cs
+++ CommandModules/help.cs
@@ -4,50 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Microsoft.Extensions.Configuration;
+using THONK.Configuration;
 
 namespace THONK.Core.CommandModules {
     [Group("help")]
-    class Help : ModuleBase<SocketCommandContext>{
-        private readonly CommandService _service;
-        private readonly IConfigurationRoot _config;
+    public class Help : ModuleBase<SocketCommandContext>{
+        
+        private readonly IConfig _config;
 
-        public Help(CommandService service, IConfigurationRoot config)
+        public Help(IConfig config)
         {
-            _service = service;
             _config = config;
         }
         [Command("")]
         public async Task Default(){
-            try {
-            string prefix = _config["prefix"];
-            var builder = new EmbedBuilder()
-            {
-                Color = new Color(114, 137, 218),
-                Description = "These are the commands you can use"
+            string p = _config[Context.Guild.Id].Prefix;
+            string[] cmds = {
+                "user",
+                "plains time",
+                "warn",
+                "kick",
+                "say",
+                "announce"
             };
-            
-            foreach (var module in _service.Modules)
-            {
-                string description = null;
-                foreach (var cmd in module.Commands)
-                {
-                    var result = await cmd.CheckPreconditionsAsync(Context);
-                    if (result.IsSuccess)
-                        description += $"{prefix}{cmd.Aliases.First()}\n";
-                }
-                
-                if (!string.IsNullOrWhiteSpace(description))
-                {
-                    builder.AddField(x =>
-                    {
-                        x.Name = module.Name;
-                        x.Value = description;
-                        x.IsInline = false;
-                    });
-                }
+            string msg = "here are the commands you can use:\n";
+            foreach(var s in cmds){
+                msg += $"{p}{s}\n";
             }
-            await Context.Channel.SendMessageAsync("", false, builder.Build());
-        }catch(Exception e){Console.WriteLine(e.ToString());}}
+            msg += "type in one of the commands for further usage help";
+            await Context.Channel.SendMessageAsync(msg);
+        }
     }
 }
